aae5cffcdc31a66a4404917fb6ce75d7bb76ad80
Miguel Angel <m.hornos@sapalomera.cat>
2024-12-03 19:54:36 +0100

    env

---
+++ env-example.php
@@ -0,0 +1,14 @@
+<!-- Miguel Angel Hornos Granda -->
+
+<?php
+define('DB_VAR', [
+    'DB_HOST' => '',
+    'DB_NAME' => '',
+    'DB_USER' => '',
+    'DB_PASSWORD' => '',
+]);
+
+define('BASE_PATH', dirname(__DIR__) . '/');
+
+define("clauSecreta", "");
+?>
